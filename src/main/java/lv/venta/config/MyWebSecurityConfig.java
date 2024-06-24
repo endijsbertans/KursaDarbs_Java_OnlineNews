61672b9398fd3fd9efdc154c4bc0b0e25f1ee3ad
Rudolfs <rudolfstaurins76@gmail.com>
2024-06-24 19:29:05 +0300

    some fixes

--- src/main/java/lv/venta/config/MyWebSecurityConfig.java
+++ src/main/java/lv/venta/config/MyWebSecurityConfig.java
@@ -37,46 +37,49 @@ public class MyWebSecurityConfig {
 
         http.authorizeHttpRequests(auth -> auth
 
-                        .requestMatchers("/h2-console/**").hasAuthority("ADMIN")
-                        .requestMatchers("/").permitAll()
-                        .requestMatchers("/h2-console").permitAll()
-                        .requestMatchers("/event/show/all").permitAll()
-                        .requestMatchers("/event/remove/{id}").permitAll()
-                        .requestMatchers("/event/add").hasAnyAuthority("ADMIN","USER")
-                        .requestMatchers("/event/update/*").permitAll()
-                        .requestMatchers("/article/show/all").permitAll()
-                        .requestMatchers("/article/remove/*").permitAll()
-                        .requestMatchers("/article/add/**").permitAll()
-                        .requestMatchers("/article/update/*").permitAll()
-                        .requestMatchers("/article/articleById/*").permitAll()
-                        .requestMatchers("/advertisement/show/all").permitAll()
-                        .requestMatchers("/advertisement/remove/*").hasAnyAuthority("ADMIN","USER")
-                        .requestMatchers("/advertisement/add").hasAuthority("ADMIN")
-                        .requestMatchers("/advertisement/update/*").hasAuthority("ADMIN")
-                        .requestMatchers("/editor/all").hasAuthority("ADMIN")
-                        .requestMatchers("/editor/remove/*").hasAuthority("ADMIN")
-                        .requestMatchers("/editor/add").hasAuthority("ADMIN")
-                        .requestMatchers("/editor/update/*").hasAuthority("ADMIN")
+                .requestMatchers("/h2-console/**").hasAuthority("ADMIN")
+                .requestMatchers("/").permitAll()
+                .requestMatchers("/h2-console").hasAnyAuthority("ADMIN","USER")
+                .requestMatchers("/event/show/all").permitAll()
+                .requestMatchers("/event/remove/{id}").hasAnyAuthority("ADMIN","USER")
+                .requestMatchers("/event/add").hasAnyAuthority("ADMIN","USER")
+                .requestMatchers("/event/update/*").hasAnyAuthority("ADMIN","USER")
 
-                        .requestMatchers("/joke/all").permitAll()
-                        .requestMatchers("/joke/remove/**").hasAuthority("ADMIN")
-                        .requestMatchers("/joke/update/**").hasAuthority("ADMIN")
-                        .requestMatchers("/joke/add").hasAuthority("ADMIN")
-                        .requestMatchers("/h2-console/**").permitAll()
-                        .requestMatchers("/error/**").permitAll()
+                .requestMatchers("/article/show/all").permitAll()
+                .requestMatchers("/article/remove/*").hasAnyAuthority("ADMIN","USER")
+                .requestMatchers("/article/add/**").hasAnyAuthority("ADMIN","USER")
+                .requestMatchers("/article/update/*").hasAnyAuthority("ADMIN","USER")
+                .requestMatchers("/article/articleById/*").permitAll()
 
+                .requestMatchers("/advertisement/show/all").permitAll()
+                .requestMatchers("/advertisement/remove/*").hasAnyAuthority("ADMIN","USER")
+                .requestMatchers("/advertisement/add").hasAuthority("ADMIN")
+                .requestMatchers("/advertisement/update/*").hasAuthority("ADMIN")
 
-                        .requestMatchers("/h2-console/**").permitAll()
-                        .requestMatchers("/review/remove/*").permitAll()
-                        .requestMatchers("/review/add/**").permitAll()
-                        .requestMatchers("/review/update/**").permitAll()
-                        .requestMatchers("/error/**").permitAll()
+                .requestMatchers("/editor/all").hasAuthority("ADMIN")
+                .requestMatchers("/editor/remove/*").hasAuthority("ADMIN")
+                .requestMatchers("/editor/add").hasAuthority("ADMIN")
+                .requestMatchers("/editor/update/*").hasAuthority("ADMIN")
+
+                .requestMatchers("/joke/all").permitAll()
+                .requestMatchers("/joke/remove/**").hasAuthority("ADMIN")
+                .requestMatchers("/joke/update/**").hasAuthority("ADMIN")
+                .requestMatchers("/joke/add").hasAuthority("ADMIN")
+                .requestMatchers("/h2-console/**").permitAll()
+                .requestMatchers("/error/**").permitAll()
 
 
+                .requestMatchers("/h2-console/**").hasAnyAuthority("ADMIN","USER")
+                .requestMatchers("/review/remove/*").permitAll()
+                .requestMatchers("/review/add/**").permitAll()
+                .requestMatchers("/review/update/**").permitAll()
+                .requestMatchers("/error/**").permitAll()
 
-                        .requestMatchers("weather").permitAll()
 
-                );
+
+                .requestMatchers("weather").permitAll()
+
+        );
 
         http.formLogin(form -> form.permitAll());
         http.csrf(csrf-> csrf.disable());
